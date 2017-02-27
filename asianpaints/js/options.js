5569a3e3db5f6f6ba8a16cc31df118f4634f82e0
Chetan Sachdev <chetan@plantyouridea.com>
2017-02-27 23:17:50 +0530

    fixed the images path

--- asianpaints/js/options.js
+++ asianpaints/js/options.js
@@ -1,78 +1,78 @@
 var options = [{
-    "thumbnail": "/asianpaints/images/opt1.png",
-    "bigImage": "/asianpaints/images/opt1-big.png",
+    "thumbnail": "images/opt1.png",
+    "bigImage": "images/opt1-big.png",
     "timestamp": 60,
-    "fullImage": "/asianpaints/images/opt1.png",
+    "fullImage": "images/opt1.png",
     "heading": "NILAYA NENA - ROCK FACE WO67D42OB75",
     "description": "Description: Lorem ipsum dolor sit amet, sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem."
 }, {
-    "thumbnail": "/asianpaints/images/opt2.png",
-    "bigImage": "/asianpaints/images/opt1-big.png",
+    "thumbnail": "images/opt2.png",
+    "bigImage": "images/opt1-big.png",
     "timestamp": 120,
-    "fullImage": "/asianpaints/images/opt2.png",
+    "fullImage": "images/opt2.png",
     "heading": "Second item",
     "description": "second item description"
 }, {
-    "bigImage": "/asianpaints/images/opt1-big.png",
-    "thumbnail": "/asianpaints/images/opt3.png",
+    "bigImage": "images/opt1-big.png",
+    "thumbnail": "images/opt3.png",
     "timestamp": 180,
-    "fullImage": "/asianpaints/images/opt3.png",
+    "fullImage": "images/opt3.png",
     "heading": "Third item",
     "description": "third item description"
 }, {
-    "bigImage": "/asianpaints/images/opt1-big.png",
-    "thumbnail": "/asianpaints/images/opt4.png",
+    "bigImage": "images/opt1-big.png",
+    "thumbnail": "images/opt4.png",
     "timestamp": 240,
-    "fullImage": "/asianpaints/images/opt4.png",
+    "fullImage": "images/opt4.png",
     "heading": "Fourth item",
     "description": "Fourth item description"
 }, {
-    "bigImage": "/asianpaints/images/opt1-big.png",
-    "thumbnail": "/asianpaints/images/opt5.png",
+    "bigImage": "images/opt1-big.png",
+    "thumbnail": "images/opt5.png",
     "timestamp": 300,
-    "fullImage": "/asianpaints/images/opt5.png",
+    "fullImage": "images/opt5.png",
     "heading": "Fifth item",
     "description": "fifth item description"
 }, {
-    "thumbnail": "/asianpaints/images/opt1.png",
-    "bigImage": "/asianpaints/images/opt1-big.png",
+    "thumbnail": "images/opt1.png",
+    "bigImage": "images/opt1-big.png",
     "timestamp": 360,
-    "fullImage": "/asianpaints/images/opt1.png",
+    "fullImage": "images/opt1.png",
     "heading": "NILAYA NENA - ROCK FACE WO67D42OB75",
     "description": "Description: Lorem ipsum dolor sit amet, sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem."
 }, {
-    "thumbnail": "/asianpaints/images/opt2.png",
-    "bigImage": "/asianpaints/images/opt1-big.png",
+    "thumbnail": "images/opt2.png",
+    "bigImage": "images/opt1-big.png",
     "timestamp": 420,
-    "fullImage": "/asianpaints/images/opt2.png",
+    "fullImage": "images/opt2.png",
     "heading": "Second item",
     "description": "second item description"
 }, {
-    "bigImage": "/asianpaints/images/opt1-big.png",
-    "thumbnail": "/asianpaints/images/opt3.png",
+    "bigImage": "images/opt1-big.png",
+    "thumbnail": "images/opt3.png",
     "timestamp": 480,
-    "fullImage": "/asianpaints/images/opt3.png",
+    "fullImage": "images/opt3.png",
     "heading": "Third item",
     "description": "third item description"
 }, {
-    "bigImage": "/asianpaints/images/opt1-big.png",
-    "thumbnail": "/asianpaints/images/opt4.png",
+    "bigImage": "images/opt1-big.png",
+    "thumbnail": "images/opt4.png",
     "timestamp": 540,
-    "fullImage": "/asianpaints/images/opt4.png",
+    "fullImage": "images/opt4.png",
     "heading": "Fourth item",
     "description": "Fourth item description"
 }, {
-    "bigImage": "/asianpaints/images/opt1-big.png",
-    "thumbnail": "/asianpaints/images/opt5.png",
+    "bigImage": "images/opt1-big.png",
+    "thumbnail": "images/opt5.png",
     "timestamp": 600,
-    "fullImage": "/asianpaints/images/opt5.png",
+    "fullImage": "images/opt5.png",
     "heading": "Fifth item",
     "description": "fifth item description"
 },{
-    "bigImage": "/asianpaints/images/opt1-big.png",
-    "thumbnail": "/asianpaints/images/opt5.png",
+    "bigImage": "images/opt1-big.png",
+    "thumbnail": "images/opt5.png",
     "timestamp": 600,
-    "fullImage": "/asianpaints/images/opt5.png",
+    "fullImage": "images/opt5.png",
     "heading": "Fifth item",
     "description": "fifth item description"
 }];
